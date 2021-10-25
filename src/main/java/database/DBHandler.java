ea659081855ad04cded6d2eea65be4505409c439
Daina Bogdanova <daina.bogdanova@gmail.com>
2021-10-25 14:55:41 +0300

    Final

--- src/main/java/database/DBHandler.java
+++ src/main/java/database/DBHandler.java
@@ -36,3 +36,36 @@ public class DBHandler {
             return connection;
         }
     }
+
+
+//  public class DBHandler {
+//  String connectionUrl = "jdbc:mysql://localhost:3306/shop";
+//  String user = "root";
+//  String pass = "root";
+
+// private static Connection connection;
+
+//public DBHandler() {
+//    try {
+//        Connection connection = DriverManager.getConnection(connectionUrl, user, pass);
+
+//        System.out.println("Connection to database successful");
+//     } catch (SQLException e) {
+//         e.printStackTrace();
+//         System.out.println("Error connection to database");
+//     }
+//    }
+
+//   public Connection getConnection() {
+//      return connection;
+//}
+
+//      String query="CREATE TABLE products (id int primary key auto_increment,"+
+//         "name VARCHAR(30) NOT NULL,"+
+//                    "PRICE FLOAT NOT NULL,"+
+//                    "quantity INT NOT NULL,"+
+//                    "description VARCHAR(50))";
+//            PreparedStatement statement=connection.prepareStatement(query);
+//            statement.execute();
+
+// }
